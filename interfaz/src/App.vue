72a9613a667024100d4549e0b14ce6f75f32e121
Juan Antonio Villoria del Moral <juvi.13.13@gmail.com>
2021-11-19 12:16:20 +0100

    Añadido vuetify

--- interfaz/src/App.vue
+++ interfaz/src/App.vue
@@ -1,22 +1,32 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="warning"
+      dark
+    >
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style lang="scss">
 #nav {
   padding: 30px;
 
